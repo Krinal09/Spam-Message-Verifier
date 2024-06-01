0dd32faa4d9e75e1a30cd12150d7d86c344cb2aa
Krinal09 <104003095+Krinal09@users.noreply.github.com>
2024-06-02 01:54:55 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,20 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+
+# Set NLTK data path to a common location
+nltk.data.path.append("/usr/share/nltk_data")
+
+# Download necessary NLTK data
+nltk.download('punkt', download_dir="/usr/share/nltk_data")
+nltk.download('stopwords', download_dir="/usr/share/nltk_data")
 
 ps = PorterStemmer()
 
-# Stremming
-
+# Stemming
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
@@ -33,23 +39,22 @@ def transform_text(text):
             
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
-
+# Load the vectorizer and model
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 st.title("Spam Message Verifier System")
 
 input_msg = st.text_area("Enter your message below:")
-    
-if st.button('Click here to check'):
 
-    # 1. preprocess
+if st.button('Click here to check'):
+    # 1. Preprocess
     transformed_msg = transform_text(input_msg)
     
-    # 2. vectorize
+    # 2. Vectorize
     vector_input = tfidf.transform([transformed_msg])
     
-    # 3. predict
+    # 3. Predict
     result = model.predict(vector_input)[0]
     
     # 4. Display
